33f6bdcad958c4530248a8e8247db7f9698c6313
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-06-01 22:39:57 -0300

    Criando componente navbar

---
+++ src/app/_components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {
+    this.mensagem();
+  }
+
+  mensagem() {
+    console.log('componente inicializou');
+  }
+}
