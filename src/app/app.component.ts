8cc5582a862ad63f7a2ef19930cbd888f011f57b
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-06-04 08:17:52 -0300

    Estilizando o estado focus dos inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './_components/navbar/navbar.component';
+import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
+import { CertificadosComponent } from "./pages/certificados/certificados.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [NavbarComponent, BaseUiComponent, CertificadosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
