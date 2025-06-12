bf4cc741d99fb187213c69f993ccf8d5a862016a
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-06-11 23:17:26 -0300

    Validando os campos do formulário

--- src/app/_components/item-certificado/item-certificado.component.ts
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { SecondaryButtonComponent } from "../secondary-button/secondary-button.component";
-import { RouterLink } from '@angular/router';
+import { SecondaryButtonComponent } from '../secondary-button/secondary-button.component';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificado',
   imports: [SecondaryButtonComponent, RouterLink],
   templateUrl: './item-certificado.component.html',
-  styleUrl: './item-certificado.component.css'
+  styleUrl: './item-certificado.component.css',
 })
 export class ItemCertificadoComponent {
-  id: string = '6'
+  id: string = '6';
+
+  constructor(private router: Router) {}
+
+  redirecionaCertificado() {
+    this.router.navigate(['/certificados', 2]);
+  }
 }
--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
-import { PrimaryButtonComponent } from "../../_components/primary-button/primary-button.component";
+import { FormsModule, NgModel } from '@angular/forms';
+import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
 
 @Component({
-  selector: 'app-certificado-form',
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
+  selector: 'app-certificados-form',
+  imports: [
+    PrimaryButtonComponent,
+    SecondaryButtonComponent,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './certificado-form.component.html',
-  styleUrl: './certificado-form.component.css'
+  styleUrl: './certificado-form.component.css',
 })
-export class CertificadoFormComponent {
+export class CertificadosFormComponent {
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React', 'Vue'];
 
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched;
+  }
 }
