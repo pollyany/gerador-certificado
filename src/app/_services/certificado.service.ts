0416e7c4854fb70c84e1056406513c8bb4b3287e
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-06-16 19:33:59 -0300

    armazenando as informações dos certificados

---
+++ src/app/_services/certificado.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Certificado } from '../interfaces/certificado';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CertificadoService {
+  certificados: Certificado[] = [];
+
+  constructor() {}
+
+  adicionarCertificado(certificado: Certificado) {
+    this.certificados.push(certificado);
+  }
+}
